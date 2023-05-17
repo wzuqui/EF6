0c01c481f3bb050f36a9b5cfc9f0dbfa73c68568
Willian Zuqui <willian.zuqui@ravex.com.br>
2023-05-16 23:33:33 -0300

    ef6

---
+++ Source/ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using Infra.Contexts;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+var xBuilder = WebApplication.CreateBuilder();
+{
+    SqlServerTypes.Utilities.LoadNativeAssemblies(AppDomain.CurrentDomain.BaseDirectory);
+    ApplicationContext.ConnectionString = xBuilder.Configuration.GetConnectionString("DefaultConnection");
+}
+
+var xApp = xBuilder.Build();
+{
+    using var xContext = new ApplicationContext();
+    var xItens = xContext.HierarquiasOrganizacionais
+        .Include(p => p.Unidades)
+        .ToList();
+
+    xItens.ForEach(p =>
+    {
+        Console.WriteLine($"Id={p.Id}" +
+                          $", Nome=`{p.Nome}`" +
+                          $", Geolocalizacao=`{p.Geolocalizacao}`" +
+                          $", HierarquiaId=`{p.HierarquiaId}`");
+
+        foreach (var xUnidade in p.Unidades)
+        {
+            Console.WriteLine($"\tId={xUnidade.Id}" +
+                              $", Nome=`{xUnidade.Nome}`" +
+                              $", HierarquiaId=`{xUnidade.HierarquiaId}`");
+        }
+    });
+}
+xApp.RunAsync();
